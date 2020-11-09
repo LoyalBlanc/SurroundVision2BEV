d8d566b4c8b9bd01417a1094684c0ae0a7556cd0
Gao Zhong <1931604@tongji.edu.cn>
2020-11-09 15:27:58 +0800

    TiEV test1109 - Failed

--- camera.py
+++ camera.py
@@ -6,58 +6,49 @@ class Camera(object):
     def __init__(self, camera_param, original_resolution, target_resolution):
         self.camera_matrix = camera_param['K']
         self.dist_coefficient = camera_param['D']
-        self.rotation = camera_param['R']
-        self.translation = camera_param['T']
-
+        self.transform = camera_param['T']
         self.original_resolution = original_resolution
         self.target_resolution = target_resolution
-        self.camera_target = np.array([[100., 0., original_resolution[0] / 2.],
-                                       [0., 100., original_resolution[1] / 2.],
-                                       [0., 0., 1.]])
 
-        self.map = self.calculate_rectify(
-            self.camera_matrix, self.dist_coefficient, self.camera_target, self.original_resolution)
-        self.warp = self.calculate_homogeneous(
-            self.camera_matrix, self.rotation, self.translation, np.matrix([[0], [0], [-1]]))
+        self.map = self.calculate_rectify(self.camera_matrix, self.dist_coefficient, self.original_resolution)
+        self.warp = self.calculate_homogeneous()
 
     @staticmethod
-    def calculate_rectify(camera_matrix, dist_coefficient, camera_target, original_resolution):
-        return cv2.fisheye.initUndistortRectifyMap(
-            camera_matrix, dist_coefficient, np.eye(3),
-            camera_target, original_resolution, cv2.CV_32F)
+    def calculate_rectify(camera_matrix, dist_coefficient, original_resolution):
+        camera_target = np.array([[100., 0., original_resolution[0] / 2.],
+                                  [0., 100., original_resolution[1] / 2.],
+                                  [0., 0., 1.]])
 
-    @staticmethod
-    def calculate_homogeneous(camera_matrix, rotation, translation, n):
-        return camera_matrix * (rotation + translation * n.T) * camera_matrix.I
+        return cv2.fisheye.initUndistortRectifyMap(camera_matrix, dist_coefficient, np.eye(3),
+                                                   camera_target, original_resolution, cv2.CV_32F)
+
+    def calculate_homogeneous(self):
+        rotation = self.transform[:3, :3]
+        translation = self.transform[:3, 3:4]
+        n = rotation.I @ np.matrix([[0], [0], [-1.0]])
+        d = 1.5 - translation.I @ np.matrix([[0], [0], [-1.0]])
+        return self.camera_matrix @ (rotation - translation @ n.T / d) @ self.camera_matrix.I
 
     def __call__(self, img):
-        # img = cv2.remap(img, self.map[0], self.map[1], cv2.INTER_LINEAR)
+        img = cv2.remap(img, self.map[0], self.map[1], cv2.INTER_LINEAR)
         img = cv2.warpPerspective(img, self.warp, self.target_resolution)
         return img
 
-    def fine_tine(self, x=0., y=0., z=0., roll=0., pitch=0., yaw=0.):
-        """ approximate new RT """
+    def fine_tine(self, x=0., y=0., z=0., roll=0., pitch=0., yaw=0., clear_mode=True):
         roll_rotation = np.matrix([[1, 0, 0],
                                    [0, np.cos(roll), -np.sin(roll)],
                                    [0, np.sin(roll), np.cos(roll)]])
-
         pitch_rotation = np.matrix([[np.cos(pitch), 0, -np.sin(pitch)],
                                     [0, 1, 0],
                                     [np.sin(pitch), 0, np.cos(pitch)]])
-
         yaw_rotation = np.matrix([[np.cos(yaw), -np.sin(yaw), 0],
                                   [np.sin(yaw), np.cos(yaw), 0],
                                   [0, 0, 1]])
 
-        # rotation = (roll_rotation * pitch_rotation * yaw_rotation)
-        # translation = np.matrix([[- x], [- y], [- z]])
-        # warp_n = rotation.I @ np.matrix([[0], [0], [-1]])
-        # self.warp = self.calculate_homogeneous(self.camera_matrix, self.rotation, self.translation, warp_n)
-        # self.fine = self.calculate_homogeneous(self.camera_matrix, rotation, translation, np.matrix([[0], [0], [-1]]))
+        rotation = (roll_rotation * pitch_rotation * yaw_rotation)
+        translation = np.matrix([[x], [y], [z]])
+        new_transform = np.concatenate((np.concatenate((rotation, translation), axis=1),
+                                        np.matrix([0, 0, 0, 1])), axis=0)
 
-        self.rotation = (roll_rotation * pitch_rotation * yaw_rotation) @ self.rotation
-        self.translation = np.matrix([[self.translation[0].item() - x],
-                                      [self.translation[1].item() - y],
-                                      [self.translation[2].item() - z]])
-        warp_n = np.matrix([[0], [0], [-1]])
-        self.warp = self.calculate_homogeneous(self.camera_matrix, self.rotation, self.translation, warp_n)
+        self.transform = new_transform if clear_mode else new_transform @ self.transform
+        self.warp = self.calculate_homogeneous()
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import numpy as np
 import cv2
 from camera import Camera
+from camera_params.bus import *
 
 if __name__ == "__main__":
-    from camera_params.bus import *
-
     cam = Camera(RIGHT, ORIGINAL_RESOLUTION, TARGET_RESOLUTION)
-    image = cv2.imread("input/cam3.jpg")
+    image = cv2.imread("input/cam0.jpg")
 
     image1 = cam(image)
+    # image1 = cv2.resize(image, TARGET_RESOLUTION)
 
-    cam.fine_tine(x=0.1, y=0.1, z=0.1, pitch=0.1, yaw=0.1)
+    cam.fine_tine(0, 0, 10, 0, 0, 0, clear_mode=False)
     image2 = cam(image)
+    cv2.imshow("test", np.concatenate((image1, image2), axis=1))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     cv2.imwrite("output/test.jpg", np.concatenate((image1, image2), axis=1))
